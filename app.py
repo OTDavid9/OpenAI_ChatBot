49c7ccef7dfe698baad0ded7e32e606e28ed8db1
Obidokun Tunji <tunjiobidokun@gmail.com>
2023-12-21 20:42:10 -0800

    updates

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify, render_template
-# from flask_cors import CORS
+from flask_cors import CORS
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
 import logging
 
 app = Flask(__name__)
-# CORS(app)l
+CORS(app)
 @app.route('/')
 def home():
     return render_template('/index.html')
