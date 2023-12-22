2c70af4b86e01b75549a6e47633e38a2b9abd40d
Obidokun Tunji <tunjiobidokun@gmail.com>
2023-12-21 19:45:00 -0800

    commented app.run for deployment

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def gpt_request():
         logging.error(f"An error occurred: {str(e)}")
         return jsonify({"success": False, "error": "An unexpected error occurred."})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
